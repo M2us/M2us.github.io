0a08106439f81cdff0036f94a60e23c6af7aabd9
M2us <Dead_Maus@mail.ru>
2019-09-29 19:03:51 +0300

    added vue js file

---
+++ js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#main',
+  data: {
+    title: 'WoWruRU Project',
+    titleDescription: 'Русификация World of Warcraft 1.12.1',
+    client: { title: 'Клиент', link: 'https://wowruru.github.io/client.html' },
+    addons: { title: 'Аддоны', link: 'https://wowruru.github.io/addons.html' },
+    server: { title: 'Серверная русификация', linl: 'https://github.com/WoWruRU/classicdb_ruRU' },
+    maintained: { title: ['WoWruRU Project maintained by', 'WoWruRU'], link: 'https://github.com/WoWruRU' },
+    vkLink: 'https://vk.com/wowruru',
+  }
+})
