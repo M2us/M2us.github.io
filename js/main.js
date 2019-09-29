638e5afe608db743d5ef906a48d8cd38c5950bc0
M2us <Dead_Maus@mail.ru>
2019-09-29 19:30:54 +0300

    added content block

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 new Vue({
-  el: '#main',
+  el: '#main', 
   data: {
     title: 'WoWruRU Project',
     titleDescription: 'Русификация World of Warcraft 1.12.1',
-    client: { title: 'Клиент', link: 'https://wowruru.github.io/client.html' },
-    addons: { title: 'Аддоны', link: 'https://wowruru.github.io/addons.html' },
-    server: { title: 'Серверная русификация', linl: 'https://github.com/WoWruRU/classicdb_ruRU' },
+    content: [
+      { title: 'Клиент', link: 'https://wowruru.github.io/client.html' },
+      { title: 'Аддоны', link: 'https://wowruru.github.io/addons.html' },
+      { title: 'Серверная русификация', link: 'https://github.com/WoWruRU/classicdb_ruRU' },
+    ],
     maintained: { title: ['WoWruRU Project maintained by', 'WoWruRU'], link: 'https://github.com/WoWruRU' },
     vkLink: 'https://vk.com/wowruru',
   }
